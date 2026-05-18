a1b71980469de6e4467ed1b0c1b0a955ed515444
Alan Jian <jian@ischool.berkeley.edu>
2026-05-18 15:07:26 -0700

    finished string library mvp

--- dynamic_string.c
+++ dynamic_string.c
@@ -43,17 +43,35 @@ string *get_slice(String *str)
     return create_string(str->str, str->len);
 };
 
+void free_String(String *str)
+{
+    // free function for strings
+    free(str->str);
+    free(str);
+}
+
+void free_string(string *str)
+{
+    free(str->str);
+    free(str);
+}
+
 void append(String *Str, string *str)
 {
 
     // Edits first string in place, appending str to the end of Str
 
-    Str->len = Str->len + str->len;
-    while (Str->len > Str->capacity)
+    int new_len = Str->len + str->len;
+    if (new_len > Str->capacity)
     {
-        Str->capacity *= 2;
-    };
+        while (new_len > Str->capacity)
+        {
+            Str->capacity *= 2;
+        };
+        Str->str = realloc(Str->str, Str->capacity);
+    }
     memcpy(&(Str->str)[Str->len], str->str, str->len);
+    Str->len = Str->len + str->len;
 };
 
 void print(string *str, ...)
--- test.c
+++ test.c
@@ -10,7 +10,7 @@ Tests the functionality of the string library.
 
 int main(void)
 {
-    char *str = "Hello, World!";
+    char *str = "Hello, World!\n";
     String *my_Str = create_String(str, strlen(str));
     printf("My String length: %i\n", my_Str->len);
     printf("My String says: %s\n", my_Str->str);
@@ -18,4 +18,19 @@ int main(void)
     string *my_str = get_slice(my_Str);
     printf("My string length: %i\n", my_str->len);
     printf("My string says: %s\n", my_str->str);
+
+    printf("Simple print wrapper says: ");
+    print(my_str);
+
+    printf("Let's try a pretty print: \n");
+    int value = 5;
+    char *str2 = "Here is my five: %i\n";
+    String *pretty_str = create_String(str2, strlen(str2));
+    string *pretty_str_slice = get_slice(pretty_str);
+    print(pretty_str_slice, value);
+
+    printf("Let's try an append: \n");
+    append(my_Str, my_str);
+    print(get_slice(my_Str));
+    printf("Capacity is now: %i", my_Str->capacity);
 };
